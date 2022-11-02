27763b3ca33d7eaab17e32284e334e9cdf415be6
Дробышев Иван <gufinhaly@gmail.com>
2022-11-02 16:41:05 +0300

    workflow corrections

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -8,7 +8,7 @@ from .filters import IngredientFilter, RecipeFilter
 from .models import Ingredient, Recipe, Tag
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (IngredientSerializer, RecipeSerializer,
-                          TagSerializer,UserRecipeSerializer)
+                          TagSerializer, UserRecipeSerializer)
 from .shopping_list_generator import generate_shopping_list
 
 User = get_user_model()
